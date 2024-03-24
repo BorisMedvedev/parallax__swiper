ad38b18e6229489b684e6aafa4eb28cd2094c0db
John Doe <johndoe@example.com>
2024-03-25 02:25:02 +0300

    fix

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,36 @@
 import '../scss/style.scss';
-import '../../node_modules/swiper/swiper-bundle.css'
+import '/node_modules/swiper/swiper-bundle.css';
 
 import Swiper from 'swiper';
-import { Navigation, Pagination, Scrollbar } from 'swiper/modules';
+import {
+  Navigation,
+  // Pagination,
+  // Scrollbar,
+  Parallax,
+  Mousewheel,
+  Controller,
+} from 'swiper/modules';
+Swiper.use([Parallax, Mousewheel, Controller, Navigation]);
 
-
-const swiper = new Swiper('.slider-parallax', {
-  modules: [Navigation, Pagination, Scrollbar],
+const swiperIMG = new Swiper('.slider-parallax', {
   slidesPerView: 1,
+  loop: false,
+  speed: 1800,
+  parallax: true,
+});
+
+const swiperText = new Swiper('.slider-text', {
+  slidesPerView: 1,
+  loop: false,
+  speed: 1800,
+  mousewheel: {
+    invert: false,
+  },
 
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
   },
-  });
+});
+
+swiperText.controller.control = swiperIMG;
