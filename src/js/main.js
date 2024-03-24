934df0f51059b4d56f547ac98043724b09fbdbb5
John Doe <johndoe@example.com>
2024-03-24 21:13:59 +0300

    fix

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,16 @@
-import '../scss/style.scss'
+import '../scss/style.scss';
+import '../../node_modules/swiper/swiper-bundle.css'
+
+import Swiper from 'swiper';
+import { Navigation, Pagination, Scrollbar } from 'swiper/modules';
+
+
+const swiper = new Swiper('.slider-parallax', {
+  modules: [Navigation, Pagination, Scrollbar],
+  slidesPerView: 1,
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  });
