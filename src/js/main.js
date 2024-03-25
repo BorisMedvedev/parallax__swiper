a9321ee7d8ae44890d514415cd5cc2081c09e7f8
“Boris <bmedved78@mail.ru>
2024-03-25 14:12:17 +0300

    fix

--- src/js/main.js
+++ src/js/main.js
@@ -2,35 +2,62 @@ import '../scss/style.scss';
 import '/node_modules/swiper/swiper-bundle.css';
 
 import Swiper from 'swiper';
+import { gsap, Power2 } from 'gsap';
 import {
   Navigation,
-  // Pagination,
-  // Scrollbar,
+  Pagination,
+  Scrollbar,
   Parallax,
   Mousewheel,
   Controller,
 } from 'swiper/modules';
-Swiper.use([Parallax, Mousewheel, Controller, Navigation]);
+Swiper.use([
+  Parallax,
+  Mousewheel,
+  Controller,
+  Navigation,
+  Pagination,
+  Scrollbar,
+]);
 
 const swiperIMG = new Swiper('.slider-parallax', {
-  slidesPerView: 1,
   loop: false,
-  speed: 1800,
+  speed: 1500,
   parallax: true,
 });
 
 const swiperText = new Swiper('.slider-text', {
-  slidesPerView: 1,
   loop: false,
-  speed: 1800,
+  speed: 1500,
+  parallax: true,
   mousewheel: {
     invert: false,
   },
-
+  pagination: {
+    el: '.slider-pagination',
+    clickable: true,
+  },
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
+  scrollbar: {
+    el: '.slider-scrollbar',
+  },
 });
 
 swiperText.controller.control = swiperIMG;
+
+const gear = document.querySelector('.slider-gear');
+swiperText.on('slideNextTransitionStart', () => {
+  gsap.to(gear, 2, {
+    rotate: '+=40',
+    ease: Power2.easeOut
+  });
+});
+swiperText.on('slidePrevTransitionStart', () => {
+  gsap.to(gear, 2, {
+    rotate: '-=40',
+    ease: Power2.easeOut
+  });
+});
